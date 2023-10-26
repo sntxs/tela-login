19f1fac57f53d9ea26b2b99be603b7e663a00d0c
sntxs <natanrodrigues649@gmail.com>
2023-10-25 21:25:35 -0400

    tela de login sem js

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <form> <!-- Formulario para efetuar o login, contendo solicitação de email e senha -->
+    <p>LOGIN</p>
+    <label for="email"> EMAIL <input type="text" id="email" required /> </label>
+    <label for="password"> SENHA <input type="password" id="password" required /> </label>
+    <button>Continuar</button>
+
+    <a href="#">Esqueci minha senha</a>
+    <a href="#">Desejo me cadastrar</a>
+  </form>
+</template>
